78e81549beda83f8c9caada3d58b6d2c32d69cad
mpdev <mpdevcake@gmail.com>
2017-09-25 10:09:40 +0300

    Adds short description to multilingual fields

--- app/models/exhibit-field.js
+++ app/models/exhibit-field.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	language: DS.attr('string'),
 	description: DS.attr('string'),
+	shortDescription: DS.attr('string'),
 	name: DS.attr('string'),
 	exhibit: DS.belongsTo('exhibit', { async: true }),
 });
